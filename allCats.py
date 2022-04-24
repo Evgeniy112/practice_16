5f5ca0ff061d5e8b8188ae18852d3e73f5b91be6
Evgeniy112 <evgeniy.pogorelov@bk.ru>
2022-04-24 12:44:40 +0300

    new

--- allCats.py
+++ allCats.py
@@ -1,6 +1,9 @@
-from cats import Cat
+from cats import Cat, Customers
 
 c1 = Cat('Барон', 'Кот', 2)
 print(c1.name, c1.age, c1.gender)
 c2 = Cat('Муся', 'Кошка', 5)
 print(c2.name, c2.age, c2.gender)
+
+customer_1 = Customers('Иван','Петров','Москва',50)
+print(customer_1)
--- cats.py
+++ cats.py
@@ -3,3 +3,12 @@ class Cat:
         self.name = name
         self.gender = gender
         self.age = age
+
+class Customers:
+    def __init__(self,first_name,second_name,city,baiance):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.balance = baiance
+        self.city = city
+    def __str__(self):
+        return f'''"{self.first_name}{self.second_name}".{self.city}. Баланс: {self.balance} руб.'''
