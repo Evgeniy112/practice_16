2ad1b13374306368cc0ade2627dd081ee8f4f9f5
Evgeniy112 <evgeniy.pogorelov@bk.ru>
2022-04-24 14:11:41 +0300

    new

--- allCats.py
+++ allCats.py
@@ -1,9 +1,16 @@
 from cats import Cat, Customers
 
 c1 = Cat('Барон', 'Кот', 2)
-print(c1.name, c1.age, c1.gender)
 c2 = Cat('Муся', 'Кошка', 5)
-print(c2.name, c2.age, c2.gender)
+pet_list = [c1,c2]
+for pet in pet_list:
+    print(pet.get_pet())
 
-customer_1 = Customers('Иван','Петров','Москва',50)
-print(customer_1)
+costomer_1 = Customers('Иван','Петров','Москва',50)
+costomer_2 = Customers('Владимир','Зайцев','Кострома',50)
+costomer_3 = Customers('Олеся','Янина','Новосибирск',50)
+
+guest_list = [costomer_1, costomer_2, costomer_3]
+
+for guest in guest_list:
+    print(guest.get_guests())
--- cats.py
+++ cats.py
@@ -3,6 +3,9 @@ class Cat:
         self.name = name
         self.gender = gender
         self.age = age
+    def get_pet(self):
+        return f'{self.name} {self.age} {self.gender}'
+
 
 class Customers:
     def __init__(self,first_name,second_name,city,baiance):
@@ -11,4 +14,6 @@ class Customers:
         self.balance = baiance
         self.city = city
     def __str__(self):
-        return f'''"{self.first_name}{self.second_name}".{self.city}. Баланс: {self.balance} руб.'''
+        return f'''"{self.first_name} {self.second_name}".{self.city}. Баланс: {self.balance} руб.'''
+    def get_guests(self):
+        return f'{self.first_name} {self.second_name} {self.city}'
